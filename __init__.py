4e0132007b0ab86d4a3e8855951b7cea6b00bd40
Oleg Denisenko <oleg.denisenko@gmail.com>
2024-01-27 18:01:04 +0200

    fix

---
+++ __init__.py
@@ -0,0 +1,50 @@
+"""Support for Awtrix time."""
+
+from __future__ import annotations
+
+import logging
+
+from homeassistant.const import Platform
+from homeassistant.helpers import (
+    device_registry as dr,
+    discovery,
+    entity_registry as er,
+)
+
+DOMAIN = "awtrix"
+
+PLATFORMS = [Platform.NOTIFY]
+
+_LOGGER = logging.getLogger(__name__)
+
+async def async_setup(hass, config):
+    """Set up the Awtrix."""
+
+    devices = []
+
+    device_registry = dr.async_get(hass)
+    entity_registry = er.async_get(hass)
+
+    for device in device_registry.devices.values():
+        ha_device_name = device.name_by_user or device.name
+        if device.manufacturer == 'Blueforcer':
+
+           device_entities = er.async_entries_for_device(
+               entity_registry,
+               device_id=device.id,
+               include_disabled_entities=False,
+           )
+
+           for entry in device_entities:
+               if 'device_topic' in entry.entity_id:
+                   devices.append({'name': ha_device_name, 'device': entry.entity_id})
+
+    for device_conf in devices:
+        hass.async_create_task(
+            discovery.async_load_platform(
+                hass, "notify", DOMAIN, device_conf, config)
+        )
+
+    # Return boolean to indicate that initialization was successfully.
+    return True
+
--- notify.py
+++ notify.py
@@ -1,67 +1,37 @@
-# -*- coding: utf-8 -*-
-
 """Support for Awtrix notifications."""
 
-import logging
 import json
+import logging
 
-from homeassistant.helpers import (
-    device_registry as dr,
-    entity_registry as er,
-)
-
-from homeassistant.components.notify import (
-    ATTR_DATA,
-    ATTR_TARGET,
-    BaseNotificationService,
-)
+from homeassistant.components.notify import ATTR_DATA, BaseNotificationService
 
 CONF_DEVICE = "device"
 
 _LOGGER = logging.getLogger(__name__)
 
-def get_awtrix_entity(hass, device_name):
-
-    device_registry = dr.async_get(hass)
-    entity_registry = er.async_get(hass)
-  
-    for device in device_registry.devices.values():
-        ha_device_name = device.name_by_user or device.name
-        if device.manufacturer == 'Blueforcer' and ha_device_name == device_name:
-
-           device_entities = er.async_entries_for_device(
-               entity_registry,
-               device_id=device.id,
-               include_disabled_entities=False,
-           )
-
-           for entry in device_entities:
-               if 'device_topic' in entry.entity_id:
-                  return entry.entity_id
-
 async def async_get_service(hass, config, discovery_info=None):
     """Get the Awtrix notification service."""
 
-    device_name = config[CONF_DEVICE]
-    # _LOGGER.warning("AWTRIX NOTIFICATION DEVICE: %s", device_name)
- 
-    entity_id = get_awtrix_entity(hass, device_name.strip())
-    if entity_id is not None:
-       return AwtrixNotificationService(entity_id)
+    if discovery_info is None:
+        return None
 
+    entity_id =  discovery_info["device"]
+    return AwtrixNotificationService(entity_id)
 
 
 ########################################################################################################
 
 class AwtrixNotificationService(BaseNotificationService):
-
     """Implement the notification service for Awtric."""
 
     def __init__(self, entity_id):
+        """Init the notification service for Awtric."""
         self.entity_id = entity_id
 
 
     async def notification(self, topic, message, data):
+        """Handle the notification service for Awtric."""
+
         icon = "/9j/4AAQSkZJRgABAQAAAQABAAD/4gHYSUNDX1BST0ZJTEUAAQEAAAHIAAAAAAQwAABtbnRyUkdCIFhZWiAH4AABAAEAAAAAAABhY3NwAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAQAA9tYAAQAAAADTLQAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAlkZXNjAAAA8AAAACRyWFlaAAABFAAAABRnWFlaAAABKAAAABRiWFlaAAABPAAAABR3dHB0AAABUAAAABRyVFJDAAABZAAAAChnVFJDAAABZAAAAChiVFJDAAABZAAAAChjcHJ0AAABjAAAADxtbHVjAAAAAAAAAAEAAAAMZW5VUwAAAAgAAAAcAHMAUgBHAEJYWVogAAAAAAAAb6IAADj1AAADkFhZWiAAAAAAAABimQAAt4UAABjaWFlaIAAAAAAAACSgAAAPhAAAts9YWVogAAAAAAAA9tYAAQAAAADTLXBhcmEAAAAAAAQAAAACZmYAAPKnAAANWQAAE9AAAApbAAAAAAAAAABtbHVjAAAAAAAAAAEAAAAMZW5VUwAAACAAAAAcAEcAbwBvAGcAbABlACAASQBuAGMALgAgADIAMAAxADb/2wBDAAEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQH/2wBDAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQH/wAARCAAIAAgDAREAAhEBAxEB/8QAFAABAAAAAAAAAAAAAAAAAAAACP/EAB8QAAECBgMAAAAAAAAAAAAAABgWFwAIFBUZKAclN//EABQBAQAAAAAAAAAAAAAAAAAAAAD/xAAUEQEAAAAAAAAAAAAAAAAAAAAA/9oADAMBAAIRAxEAPwAIsRKxgUyFXyX80lq0bFEZx0Oow1CqANlHPTviW2NBfMjhh7BPgVvZwH//2Q=="
         rtttl = "Notification:d=2,o=4,b=160:32g,32p,32g,32p,32g,32p"
 
